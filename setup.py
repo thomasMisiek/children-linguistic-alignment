d388a3c47005e7ba43ff2dd7f92d04bbdda221c9
thomasMisiek <thomas.misiek@gmail.com>
2021-04-05 19:44:40 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='children-linguistic-alignment',
+      version='1.0',
+      description='',
+      author='Thomas MISIEK',
+      author_email='thomas.misiek@gmail.com',
+      url='',
+      packages=[],
+     )
