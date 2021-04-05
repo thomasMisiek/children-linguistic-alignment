2a9c1b9c8d2f472d0dfd869fd54e91a5443d39e2
thomasMisiek <thomas.misiek@gmail.com>
2021-04-05 21:49:21 +0100

    Changed setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
-#!/usr/bin/env python
+import setuptools
 
-from distutils.core import setup
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
-setup(name='children-linguistic-alignment',
-      version='1.0',
-      description='',
-      author='Thomas MISIEK',
-      author_email='thomas.misiek@gmail.com',
-      url='',
-      packages=[],
-     )
+setuptools.setup(
+    name='children-linguistic-alignment',
+    version="0.0.1",
+    author='Thomas MISIEK',
+    author_email='thomas.misiek@gmail.com',
+    description="A small example package",
+    url="https://github.com/pypa/sampleproject",
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.x",
+)
